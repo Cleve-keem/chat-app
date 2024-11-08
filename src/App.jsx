86f377562d3bd1d78e0bb305155ba693ba6a0da4
hakeem <belloharyourmidey@gmail.com>
2024-11-08 05:14:51 -0800

    loading Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import Chat from './Components/Chat';
 import Details from './Components/Details';
 import UserList from './Components/User/UserList';
 
+
 function App() {
+  const user = false;
+  
   return (
     <div className="w-[100vw] h-[100vh] md:w-[90vw] md:h-[90vh] bg-slate-900 flex rounded-xl overflow-hidden">
-      <UserList />
-      <Chat />
-      <Details />
+      {
+        user ? (
+          <>
+            <UserList />
+            <Chat />
+            <Details />
+        
+          </>
+        ) : (<Login />)
+      }
     </div>
   )
 }
